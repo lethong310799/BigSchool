20e67d9bbe7800838d2f78127de345f5e61440c6
lethong <lethong990731@gmail.com>
2021-06-28 16:44:44 +0700

    first commit

---
+++ BigSchool/Migrations/202106280741554_Create-Category-Course.cs
@@ -0,0 +1,46 @@
+namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateCategoryCourse : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Lecturer_UserLockoutEnabledByDefault = c.Boolean(nullable: false),
+                        Lecturer_MaxFailedAccessAttemptsBeforeLockout = c.Int(nullable: false),
+                        Lecturer_DefaultAccountLockoutTimeSpan = c.Time(nullable: false, precision: 7),
+                        LecturerId = c.String(nullable: false),
+                        Place = c.String(nullable: false, maxLength: 255),
+                        DateTime = c.DateTime(nullable: false),
+                        CategoryId = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courses", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "CategoryId" });
+            DropTable("dbo.Courses");
+            DropTable("dbo.Categories");
+        }
+    }
+}
